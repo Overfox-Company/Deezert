fffdc9d49ea5ffcf1815fe3133d13488a5262309
joel <jzambranojazc2001gmail.com>
2023-01-23 18:25:01 -0400

    test0.1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,13 @@
 import React from "react";
 import Header from "../components/header/Header";
 import styles from "../styles/Home.module.css";
-import Paper from "@mui/material/Paper";
-import Switch from "../components/Switch";
-import { P } from "../components/BasicComponents";
-import GoogleLoginButotn from "../hooks/google/Google";
-import { AppContext } from "../context/AppContext";
-import Button from "@mui/material/Button";
-import { ConnectServer } from "../functions/app/ApiFunctions";
+import Login from "../Views/login/Login";
 export default function Home() {
   return (
     <>
       <Header />
       <main className={styles.main}>
-
+<Login/>
       </main>
     </>
   );

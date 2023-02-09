5e9d63bfd039c29b9b10d1f2e31e714744a97a3e
joel <jzambranojazc2001gmail.com>
2023-02-09 14:46:04 -0400

    change urls

--- src/api/config/Microservices.ts
+++ src/api/config/Microservices.ts
@@ -1,4 +1,5 @@
-export const DeezertSessions = `${"http://localhost:8000/"}Deezertsessions/api`;
-//`https://deezertsessions.onrender.com/Deezertsessions/api`;
-export const DeezertManagement = "http://localhost:8080/DeezertManagement/api";
-//"https://deezertmanagement.onrender.com/DeezertManagement/api";
+export const DeezertSessions =
+  //`${"http://localhost:8000/"}Deezertsessions/api`;
+  `https://deezertsessions.onrender.com/Deezertsessions/api`;
+export const DeezertManagement = //"http://localhost:8080/DeezertManagement/api";
+  "https://deezertmanagement.onrender.com/DeezertManagement/api";

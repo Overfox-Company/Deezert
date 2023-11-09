1d5f88f4a84e1453e1a8d2953064ec5970d52049
joel <jzambranojazc2001gmail.com>
2023-11-09 18:54:50 -0400

    adjust import avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
 import Tooltip from "@mui/material/Tooltip";
-import AvatarMaterial from "@mui/material/Avatar/Avatar";
+import AvatarMaterial from "@mui/material/Avatar";
 const AvatarImg = styled(AvatarMaterial)({
   width: 30,
   height: 30,
